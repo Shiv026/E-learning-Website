04ddda02b6d1ff714a35a0f5620c2d416b617321
Shiv Shankar <shiv@Mac.lan>
2025-07-02 04:22:26 +0530

    Removed node modules and env files from tracking

--- server/config/env.js
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-
-config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
-
-
-export const { PORT, NODE_ENV, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
